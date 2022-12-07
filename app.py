2f796fe51748b48c9b550bbef9338881d85cd3b8
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-07 23:53:40 +0300

    add

--- app.py
+++ app.py
@@ -3,16 +3,12 @@ import streamlit as st
 from transformers import pipeline
 from pydantic import BaseModel
 
-class Item(BaseModel):
-    text: str
+st.header("Определение тональности текстов")
+st.subheader("Введите текст для анализа")
 
-app = FastAPI(title= "Определение тональности текстов на русском языке")
-classifier = pipeline("sentiment-analysis","blanchefort/rubert-base-cased-sentiment")
+text = st.text_area(" ",height=100)
 
-@app.get("/")
-def root():
-    return {"message": "Привет"}
-
-@app.post("/predict/")
-def predict(item: Item):
-    return classifier(item.text )[0]
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+st.write ("Тональность текста:")
+st.write(classifier(text)[0]["label"])
