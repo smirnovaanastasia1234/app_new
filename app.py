41883defa9d2eb0e2d294ecd61c543f6bb8ff328
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-17 11:40:41 +0300

    add

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ st.subheader("Введите текст для анализа")
 text = st.text_area(" ",height=100)
 
 classifier = pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")
+                      "blanchefort/rubert-base-cased-sentiment")  
 st.write ("Тональность текста:")
 st.write(classifier(text)[0]["label"])
