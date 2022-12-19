628d80347bb9974c9d147b2b48845451c98ef720
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-19 19:22:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from transformers import pipeline
 
 @st.cache(allow_output_mutation=True)
 
-st.header("Определение тональности текстов")
-st.subheader("Введите текст для анализа")
+st.header ("Определение тональности текстов")
+st.subheader ("Введите текст для анализа")
 def load_model():
 	  return torch.load("path/to/model.pt")
 
