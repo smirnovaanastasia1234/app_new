63d00701876be64f8282ca0febd70abbc813b554
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-20 10:49:12 +0300

    changes

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ from transformers import pipeline
 @st.cache(allow_output_mutation=True)
 
 def load_model():
-	  return torch.load("path/to/model.pt")
+	  model=pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+      return model
 
 model = load_model()
-
+st.header ("Определение тональности текстов")
+st.subheader ("Введите текст для анализа")
 text = st.text_area(" ",height=100)
-
-classifier = pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")  
-
 result = st.button("Определить тональность текста")
+
+
 if result:
-    x=pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")  
+    res = model(text)
+    sent = res[0]['label'] 
     st.write(classifier(text)[0]["label"])
 
 
