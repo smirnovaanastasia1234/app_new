4e34d58da4918f1b1a30b1a7df7140d7e6c07b0a
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-21 23:26:21 +0300

    changes

--- ex.py
+++ ex.py
@@ -6,10 +6,11 @@ from transformers import BertTokenizerFast
 st.header ("Определение тональности текстов")
 st.subheader ("Введите текст для анализа")
 
+
 tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment-rurewiews')
 model = AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment-rurewiews', return_dict=True)
 
-@st.cache(allow_output_mutation=True)
+@torch.no_grad()
 def predict(text):
     inputs = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt')
     outputs = model(**inputs)
