1e1070cbe73dfb25c18f8f8b66fe8a12801d0f83
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-22 00:48:50 +0300

    changes

--- app.py
+++ app.py
@@ -31,22 +31,22 @@ if option == 'Home':
 elif option == "Определение тональности текста":
 
 
-def load_model():
+
+ @st.cache(allow_output_mutation=True)
+
+ def load_model():
     model=pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
     return model
-
-model = load_model()
-st.header ("Определение тональности текстов")
-st.subheader ("Введите текст для анализа")
-text = st.text_area(" ",height=100)
-result = st.button("Определить тональность текста")
+ model = load_model()
+ st.header ("Определение тональности текстов")
+ st.subheader ("Введите текст для анализа")
+ text = st.text_area(" ",height=100)
+ result = st.button("Определить тональность текста")
 
 
 if result:
     res = model(text)
     sent = res[0]['label'] 
     st.write(model(text)[0]["label"])
-
-
-
+ 
